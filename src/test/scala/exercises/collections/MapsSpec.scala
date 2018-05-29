103aad1318b4b74d91bed2135433d1574b13ef1e
DavidCorral94 <david.corral@uca.es>
2018-05-29 11:41:34 +0200

    Rafa files & maps tests

---
+++ src/test/scala/exercises/collections/MapsSpec.scala
@@ -0,0 +1,112 @@
+package exercises.collections
+
+import org.scalatest._
+
+class MapsSpec extends FlatSpec with Matchers {
+
+  "Map size" should "be 4" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    myMap.size should be(4)
+  }
+
+  it should "be 3" in {
+    val myMap = Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "MI" → "Michigan")
+    myMap.size should be(3)
+  }
+
+  it should "new map will have IL" in {
+
+    val myMap = Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "MI" → "Michigan")
+    val aNewMap = myMap + ("IL" → "Illinois")
+    aNewMap.contains("IL") should be(true)
+  }
+
+  it should "iterated" in {
+    val myMap = Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "MI" → "Michigan")
+
+    val mapValues = myMap.values
+    mapValues.size should be(3)
+    mapValues.head should be("Michigan") //Failed presumption: The order in maps is not guaranteed
+
+    val lastElement = mapValues.last
+    lastElement should be("Wisconsin")
+  }
+
+  it should "accesed" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    myMap("MI") should be("Michigan")
+    myMap("IA") should be("Iowa")
+  }
+
+  it should "mixed type" in {
+    val myMap = Map("Ann Arbor" → "MI", 49931 → "MI")
+    myMap("Ann Arbor") should be("MI")
+    myMap(49931) should be("MI")
+  }
+
+  it should "non-existing" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    intercept[NoSuchElementException] {
+      myMap("TX")
+    }
+    myMap.getOrElse("TX", "missing data") should be("missing data")
+
+    val myMap2 = Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa") withDefaultValue "missing data"
+    myMap2("TX") should be("missing data")
+  }
+
+  it should "element removed" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val aNewMap = myMap - "MI"
+    aNewMap.contains("MI") should be(false)
+    myMap.contains("MI") should be(true)
+  }
+
+  it should "multiple elements removed" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val aNewMap = myMap -- List("MI", "OH")
+
+    aNewMap.contains("MI") should be(false)
+    myMap.contains("MI") should be(true)
+
+    aNewMap.contains("WI") should be(true)
+    aNewMap.size should be(2)
+    myMap.size should be(4)
+  }
+
+  it should "remove tuple" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val aNewMap = myMap - ("MI", "WI") // Notice: single '-' operator for tuples
+
+    aNewMap.contains("MI") should be(false)
+    myMap.contains("MI") should be(true)
+    aNewMap.contains("OH") should be(true)
+    aNewMap.size should be(2)
+    myMap.size should be(4)
+  }
+
+  it should "removing non-existing element" in {
+    val myMap =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val aNewMap = myMap - "MN"
+
+    aNewMap.equals(myMap) should be(true)
+  }
+
+  it should "maps equals" in {
+    val myMap1 =
+      Map("MI" → "Michigan", "OH" → "Ohio", "WI" → "Wisconsin", "IA" → "Iowa")
+    val myMap2 =
+      Map("WI" → "Wisconsin", "MI" → "Michigan", "IA" → "Iowa", "OH" → "Ohio")
+
+    myMap1.equals(myMap2) should be(true)
+  }
+
+}
+
